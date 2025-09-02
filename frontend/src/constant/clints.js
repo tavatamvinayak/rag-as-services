faa7c435253baa994b1eeacb5419d04be646f9b8
vinayak tavatam <107667781+tavatamvinayak@users.noreply.github.com>
2025-09-02 17:53:01 +0530

    Update clints.js

--- frontend/src/constant/clints.js
+++ frontend/src/constant/clints.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL_NODEJS= "http://localhost:8085" || process.env.BACKEND_URL_NODEJS 
-export const BACKEND_URL_FASTAPI= "http://localhost:5000" || process.env.BACKEND_URL_FASTAPI
+export const BACKEND_URL_NODEJS= process.env.BACKEND_URL_NODEJS || "http://localhost:8085" 
+export const BACKEND_URL_FASTAPI= process.env.BACKEND_URL_FASTAPI  || "http://localhost:5000"
